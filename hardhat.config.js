57ba78da1d22e931330aea72f7802cd35a8f4f86
Kai <100889622+Kaiouu@users.noreply.github.com>
2022-04-02 03:48:59 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/cOZi2UzW-vvmsCt1sKgRI8vKgu-NQIKV",
+      accounts: ["4d2253e4a9c729188e19182498de0f79b43fa7be7de37961ccb6d18e7ffe1def"],
+    }
+  }
+};
